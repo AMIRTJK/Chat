ad7c7d220828d1a690742a764b2d95338de776bf
AMIRTJK <jsharipovamir@gmail.com>
2024-04-01 13:51:18 +0500

    3

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,6 @@ import { Button } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import { actions } from "./slices/chat-slice";
 
-import ministerImg from "./imageUrl";
-
 function App() {
   const Dispatch = useDispatch();
 
@@ -25,6 +23,8 @@ function App() {
   const handleClick = () => {
     Dispatch(setShowStructure(true));
   };
+
+  const ministerImg = "https://i.ibb.co/HqvF08R/image.jpg";
 
   return (
     <>
--- src/imageUrl.js
+++
@@ -1,4 +0,0 @@
-export const ministerImg = "https://i.ibb.co/HqvF08R/image.jpg";
-
-export const docNo1 =
-  "https://1.downloader.disk.yandex.ru/preview/e63acdfb1606ea69a96599593d1e94186fc8ddaf10a01b295380eb3b3911ea3b/inf/YvXI8oa3ol13-dKHYZz5lQ9lZHCIlEdYNMhrGtjpoDaIovFam3-1LXaiSGuygLv2qkd8vuA3uiJzjJOyeK5Hhg%3D%3D?uid=1586284750&filename=Document%E2%84%961.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1586284750&tknv=v2&size=1920x945";
