1a851a83a6605ddf83f93fd65f985e80f6290090
AMIRTJK <jsharipovamir@gmail.com>
2024-04-01 14:01:40 +0500

    Change image

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,7 @@ function App() {
   };
 
   const ministerImg = "https://i.ibb.co/HqvF08R/image.jpg";
+  const docNo1Img = "https://i.ibb.co/L1Bj3fb/Document-1.png";
 
   return (
     <>
@@ -72,8 +73,8 @@ function App() {
                   Вложенные документы
                 </p>
                 <div className="wrapper-list flex gap-4">
-                  <AttachedDocuments document="https://1.downloader.disk.yandex.ru/preview/e63acdfb1606ea69a96599593d1e94186fc8ddaf10a01b295380eb3b3911ea3b/inf/YvXI8oa3ol13-dKHYZz5lQ9lZHCIlEdYNMhrGtjpoDaIovFam3-1LXaiSGuygLv2qkd8vuA3uiJzjJOyeK5Hhg%3D%3D?uid=1586284750&filename=Document%E2%84%961.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1586284750&tknv=v2&size=1920x945" />
-                  <AttachedDocuments document="https://1.downloader.disk.yandex.ru/preview/e63acdfb1606ea69a96599593d1e94186fc8ddaf10a01b295380eb3b3911ea3b/inf/YvXI8oa3ol13-dKHYZz5lQ9lZHCIlEdYNMhrGtjpoDaIovFam3-1LXaiSGuygLv2qkd8vuA3uiJzjJOyeK5Hhg%3D%3D?uid=1586284750&filename=Document%E2%84%961.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1586284750&tknv=v2&size=1920x945" />
+                  <AttachedDocuments document={docNo1Img} />
+                  <AttachedDocuments document={docNo1Img} />
                 </div>
               </div>
             </div>
