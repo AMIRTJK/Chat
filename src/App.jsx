027c4b529481dc86afaafdd5895078fd64f15d4e
AMIRTJK <jsharipovamir@gmail.com>
2024-04-01 13:22:42 +0500

    add image from local path

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ function App() {
           >
             <div className="avatar flex items-center gap-5 mb-[30px]">
               <div className="wrapper-image w-[60px] h-[60px] rounded-[30px] overflow-hidden border-[1px] border-[#007cd2]">
-                <img src={ministerImg} alt="" />
+                <img src="src/assets/КАХХОРЗОДА ФАЙЗИДДИН САТТОР.jpg" alt="" />
               </div>
 
               <div className="text">
@@ -72,8 +72,8 @@ function App() {
                   Вложенные документы
                 </p>
                 <div className="wrapper-list flex gap-4">
-                  <AttachedDocuments document={docNo1} />
-                  <AttachedDocuments document={docNo1} />
+                  <AttachedDocuments document="https://1.downloader.disk.yandex.ru/preview/e63acdfb1606ea69a96599593d1e94186fc8ddaf10a01b295380eb3b3911ea3b/inf/YvXI8oa3ol13-dKHYZz5lQ9lZHCIlEdYNMhrGtjpoDaIovFam3-1LXaiSGuygLv2qkd8vuA3uiJzjJOyeK5Hhg%3D%3D?uid=1586284750&filename=Document%E2%84%961.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1586284750&tknv=v2&size=1920x945" />
+                  <AttachedDocuments document="https://1.downloader.disk.yandex.ru/preview/e63acdfb1606ea69a96599593d1e94186fc8ddaf10a01b295380eb3b3911ea3b/inf/YvXI8oa3ol13-dKHYZz5lQ9lZHCIlEdYNMhrGtjpoDaIovFam3-1LXaiSGuygLv2qkd8vuA3uiJzjJOyeK5Hhg%3D%3D?uid=1586284750&filename=Document%E2%84%961.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1586284750&tknv=v2&size=1920x945" />
                 </div>
               </div>
             </div>
