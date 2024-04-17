3cde63206b943e786830e975ede37a52031519e9
AMIRTJK <jsharipovamir@gmail.com>
2024-04-17 15:39:11 +0500

    fixed image

--- src/routes/Authorization.jsx
+++ src/routes/Authorization.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import logo from "../assets/logo.svg";
 import LanguageIcon from "@mui/icons-material/Language";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import { Button } from "@mui/material";
@@ -34,7 +33,11 @@ const Authorization = () => {
     <section className="bg-[#fff] w-full h-full absolute z-10">
       <header className="bg-[#fff] flex justify-between items-center py-[24px] pl-[15px]">
         <div className="wrapper-logo flex items-center gap-5 cursor-pointer ml-[193px]">
-          <img src={logo} alt="" />
+          <img
+            src="https://i.ibb.co/xCjbnnw/emblem.png"
+            alt=""
+            className="w-[4%]"
+          />
           <div className="text flex flex-col items-center">
             <p className="mulish text-[#607d8b]">Вазорати молияи</p>
             <p className="mulish font-[700] text-[#607d8b]">
